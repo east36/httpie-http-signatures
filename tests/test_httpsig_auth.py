ec640cdfdc6d3e3926dc3f5d0587ffd6caa722a0
Mike Manuthu <mmanuthu@east36.co.ke>
2017-07-26 15:26:24 +0300

    Added tox for tests. Added more tests

--- tests/test_httpsig_auth.py
+++ tests/test_httpsig_auth.py
@@ -1,5 +1,6 @@
 import unittest
 
+from httpsig.utils import parse_authorization_header
 from httpie_httpsig_auth import HTTPSignatureAuth
 
 
@@ -7,11 +8,9 @@ class TestHTTPSignatures(unittest.TestCase):
     def test_call(self):
 
         class RequestMock(object):
-            def __init__(self, headers, method, url, path_url):
-                self.headers = headers
-                self.method = method
-                self.url = url
-                self.path_url = path_url
+            def __init__(self, **kwargs):
+                for k, v in kwargs.iteritems():
+                    setattr(self, k, v)
 
         request_params = dict(
             headers={'date': 'Wed, 26 Jul 2017 11:02:39 GMT'},
@@ -20,8 +19,25 @@ class TestHTTPSignatures(unittest.TestCase):
             path_url='/')
         request_mock = RequestMock(**request_params)
         httpsig_auth = HTTPSignatureAuth('key_id', 'private_key')
-        # Make a signed request
+
+        # Make a GET signed request
         signed_request = httpsig_auth(request_mock)
-        auth = signed_request.headers['authorization']
-        assert auth is not None
-        assert auth == 'Signature headers="date (request-target) host",keyId="key_id",algorithm="hmac-sha256",signature="rrFf9OYggCCxjTCv2hIuH+OEI2gbeaFUlc1viEGQdfo="' # noqa
+        auth = parse_authorization_header(
+            signed_request.headers['authorization'])[1]
+        self.assertTrue(auth is not None)
+        self.assertTrue(
+            auth['signature'] == 'rrFf9OYggCCxjTCv2hIuH+OEI2gbeaFUlc1viEGQdfo=') # noqa
+        self.assertTrue(auth['headers'] == 'date (request-target) host')
+        self.assertTrue(auth['keyid'] == 'key_id')
+
+        # Do a POST signed request
+        request_params['method'] = 'post'
+        request_params['body'] = {}
+        request_mock = RequestMock(**request_params)
+        signed_request = httpsig_auth(request_mock)
+        auth = parse_authorization_header(
+            signed_request.headers['authorization'])[1]
+        self.assertTrue(
+            auth['headers'] == 'date (request-target) host content-length content-type digest') # noqa
+        self.assertTrue(
+            auth['signature'] == 'Y9VjBPTbnV7KprGGb2eoUC0M8XAMDK7hqVfuMh2sxMA=') # noqa
